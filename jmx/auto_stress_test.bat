6c6f0d70a271bc9e3a89ff58db4347c112379d41
qianjizhi <jizhi.qian@jimubox.com>
2021-02-16 21:40:09 +0800

    fixup

--- jmx/auto_stress_test.bat
+++ jmx/auto_stress_test.bat
@@ -1,10 +1,9 @@
 @echo off
 
-rem 1. 需要在系统变量中定义jmeter根目录的位置，如下
-rem set "jmeter_path=\your jmeter path\"
+rem 1. 需要在系统变量中定义jmeter根目录的位置，将其命名为变量 jmeter_path
 rem
-rem 2. Windows上启用sed命令的方法
-rem 需要在windows上安装git bash，并将\git bash root\usr\bin 添加到系统path变量
+rem 2. Windows上启用sed命令的方法:
+rem 需要在windows上安装git bash，并将"git bash root"\usr\bin 添加到系统path变量
 rem
 rem 3. 压测脚本模板中设定的压测时间应为20秒
 set "jmx_template=iInterface"
@@ -13,7 +12,6 @@ set "jmx_template_filename=%jmx_template%%suffix%"
 set num=10
 
 echo Start auto stress test.
-
 :auto
 rem 生成对应压测线程的jmx文件
 set "jmx_filename=%jmx_template%_%num%%suffix%"
